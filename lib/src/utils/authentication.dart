b90fbd42e16157b04eb74142563350d5bfee3eee
Lucas <lucasvp611@hotmail.com>
2022-01-28 16:58:39 -0300

    Create setup flavors file

---
+++ lib/src/utils/authentication.dart
@@ -0,0 +1,25 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Authentication {
+  static String _tokenKey = "token";
+
+  static Future<bool> authenticated() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_tokenKey) != null;
+  }
+
+  static void logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove(_tokenKey);
+  }
+
+  static void saveToken(String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(_tokenKey, token);
+  }
+
+  static Future<String> getToken() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(_tokenKey)!;
+  }
+}
