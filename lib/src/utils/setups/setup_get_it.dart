b0e8863b31f3ae9b4fc4ae5ac9edf1536167c139
Lucas <lucasvp611@hotmail.com>
2022-04-27 13:47:20 -0300

    Fix some imports missing

--- lib/src/utils/setups/setup_get_it.dart
+++ lib/src/utils/setups/setup_get_it.dart
@@ -1,10 +1,11 @@
 import 'package:get_it/get_it.dart';
-import 'package:loomi_flutter_boilerplate/src/domain/repositories/i_example_repository.dart';
-import 'package:loomi_flutter_boilerplate/src/domain/usecases/get_example_uc.dart';
-import 'package:loomi_flutter_boilerplate/src/external/datasources/example_datasource.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/stores/example_store.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/usecases/i_get_example_uc.dart';
-import 'package:loomi_flutter_boilerplate/src/utils/dio_config.dart';
+
+import '../../domain/repositories/i_example_repository.dart';
+import '../../domain/usecases/get_example_uc.dart';
+import '../../external/datasources/example_datasource.dart';
+import '../../presentation/stores/example_store.dart';
+import '../../presentation/usecases/i_get_example_uc.dart';
+import '../dio_config.dart';
 
 void setupGetIt() {
   //Stores
