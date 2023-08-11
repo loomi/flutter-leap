cc311ab55997a103a3607361725b9ff1c04b0fce
Dario415 <dariomsa415@gmail.com>
2023-08-10 23:10:38 -0300

    add comments

--- lib/src/presentation/views/home_screen.dart
+++ lib/src/presentation/views/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_leap/src/utils/custom_colors.dart';
 import 'package:flutter_leap/src/utils/helpers/loomi_date_time.dart';
-import 'package:loomi_ui_flutter/loomi_ui.dart';
 
 import '../widgets/bottom_navigation_bar.dart';
 
@@ -37,22 +36,27 @@ class _HomeScreenState extends State<HomeScreen> {
       body: SizedBox(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Stack(
           children: [
-            Text(DateTime.now().formatLastSeenDate),
-            Text(DateTime.now().add(Duration(minutes: -10)).formatLastSeenDate),
-            Text(DateTime.now().add(Duration(hours: -1)).formatLastSeenDate),
-            Text(DateTime.now().add(Duration(days: -1)).formatLastSeenDate),
-            Text(DateTime.now().add(Duration(days: 2)).formatLastSeenDate),
-            const SizedBox(
-              height: 24,
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: IndexedStack(
+                index: _currentIndex,
+                children: _children,
+              ),
             ),
-            Text(DateTime.now().formatedDateString()),
-            Text(DateTime.now().formatedDateString(format: 'dd/MMM/yyyy')),
-            Text(DateTime.now().formatedDateString(format: 'dd/MMMM/yyyy')),
-            Text(DateTime.now().formatedDateString(format: 'dd MMMM yyyy')),
-            Text(DateTime.now().formatedDateString(format: 'dd MMMM yyyy')),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: CustomBottomNavigationBar(
+                index: _currentIndex,
+                onClick: (value) {
+                  setState(() {
+                    _currentIndex = value;
+                  });
+                },
+              ),
+            ),
           ],
         ),
       ),
