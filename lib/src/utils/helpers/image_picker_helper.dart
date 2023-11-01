f660123b953a37eb6d7219b7f6924148ab3ebb5b
lucavps <lucasvp611@hotmail.com>
2023-11-01 11:07:47 -0300

    Add push to camera screen and compress image functions

--- lib/src/utils/helpers/image_picker_helper.dart
+++ lib/src/utils/helpers/image_picker_helper.dart
@@ -1,7 +1,13 @@
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+
+import 'package:camera/camera.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:loomi_ui_flutter/loomi_ui.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<File?> getImageHelper({required ImageSource source}) async {
   final ImagePicker _picker = ImagePicker();
@@ -47,4 +53,57 @@ Future<File?> getVideoHelper({
   }
 
   return null;
+}
+
+Future pushToCameraScreen({
+  required BuildContext context,
+  String? popUntil,
+  required Function(String) onFileAdded,
+  Function()? helpIconFunction,
+}) async {
+  List<CameraDescription> cameras = await availableCameras();
+
+  Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) => CameraScreen(
+        cameras: cameras,
+        helpIconFunction: helpIconFunction,
+        onSave: (picture) {
+          if (popUntil != null) {
+            Navigator.popUntil(
+              context,
+              ModalRoute.withName(popUntil),
+            );
+          }
+          onFileAdded(picture.path);
+        },
+      ),
+    ),
+  );
+}
+
+Future<File> compressAndGetFile({
+  required File file,
+  int minWidth = 1920,
+  int minHeight = 1080,
+}) async {
+  String path = (await getTemporaryDirectory()).path;
+  var result = File((await FlutterImageCompress.compressAndGetFile(
+        file.absolute.path,
+        "$path/${DateTime.now().microsecondsSinceEpoch.toString()}.png",
+        format: CompressFormat.png,
+        minHeight: minHeight,
+        minWidth: minWidth,
+      ))
+          ?.path ??
+      "");
+  if (result.path.isEmpty) {
+    result = await compressAndGetFile(
+      file: file,
+      minHeight: minHeight,
+      minWidth: minWidth,
+    );
+  }
+  return result;
 }
