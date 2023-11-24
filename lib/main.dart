ae6ef6b7ded747c2e352693de919997a7fd31d06
gabrielerego <gabriele.rego.117@ufrn.edu.br>
2023-11-24 11:36:37 -0300

    Sentry config

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_leap/src/presentation/views/splash/splash_screen.dart';
 import 'package:flutter_leap/src/presentation/widgets/offline_wrapper_component.dart';
 import 'package:flutter_leap/src/utils/localization/app_localizations.dart';
 import 'package:flutter_leap/src/utils/app_global_context.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'src/utils/custom_colors.dart';
 import 'src/utils/routes.dart';
 import 'src/utils/setups/setup_flavors.dart';
@@ -29,8 +30,16 @@ void main() async {
       statusBarColor: CustomColors.black.withOpacity(0.2),
     ),
   );
-
-  runApp(const MyApp());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://1f99b34f8bb7ef3b0a4f5b96f79d2854@o4506281261858816.ingest.sentry.io/4506281291743232';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(const MyApp()),
+  );
 }
 
 class MyApp extends StatefulWidget {
--- lib/src/presentation/views/home_screen.dart
+++ lib/src/presentation/views/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:flutter_leap/main.dart';
 import 'package:flutter_leap/src/utils/custom_colors.dart';
 import 'package:flutter_leap/src/utils/helpers/select_videos_sheet_helper.dart';
 import 'package:flutter_leap/src/utils/localization/app_localizations.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import '../widgets/bottom_navigation_bar.dart';
 
@@ -94,7 +95,16 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                         CustomButton(
                           text: "Trocar",
-                          onTap: () {
+                          onTap: () async {
+                            try {
+                              int? test;
+                              test! + 3;
+                            } catch (exception, stackTrace) {
+                              await Sentry.captureException(
+                                exception,
+                                stackTrace: stackTrace,
+                              );
+                            }
                             MyApp.setLocale(
                               context,
                               const Locale("es"),
