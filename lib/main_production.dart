2ebe30dca5b1a498f1b62d5412acf1197541782b
Italo Dias <italo-2001@hotmail.com>
2024-10-14 11:00:50 -0300

    Fix: putting sentry's dns in environments class

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_leap_v2/bootstrap.dart';
+import 'package:flutter_leap_v2/shared/utils/environments.dart';
 import 'package:flutter_leap_v2/shared/utils/flavors_options.dart';
 
 void main() async {
   bootstrap(
     flavor: FlavorsOptions.production,
-    sentryUrlDsn: "", //const String.fromEnvironment('SENTRY_DSN')
+    sentryUrlDsn: Environments.sentryUrlDsn,
   );
 }
--- lib/shared/utils/environments.dart
+++ lib/shared/utils/environments.dart
@@ -1,6 +1,9 @@
 class Environments {
   static String get baseUrl => const String.fromEnvironment('BASE_URL');
 
+  static String get sentryUrlDsn => const String.fromEnvironment(
+        'SENTRY_DSN',
+      );
   static String get authenticationEncryptionKey => const String.fromEnvironment(
         'ENCRYPTION_KEY',
       ); //create your own with => secure-random -l 16
